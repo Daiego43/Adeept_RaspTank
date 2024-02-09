57580dd31720dcf9b40fec03393125ca144716fe
Daiego43 <diedelcha@gmail.com>
2024-02-09 22:10:38 +0100

    Line Follower

--- rasptank_control/siguelinea/linefollower.py
+++ rasptank_control/siguelinea/linefollower.py
@@ -1,33 +1,32 @@
 import time
 import RPi.GPIO as GPIO
 
-
 line_pin_right = 19
 line_pin_middle = 16
 line_pin_left = 20
 
 
-def setup():
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(line_pin_right, GPIO.IN)
-    GPIO.setup(line_pin_middle, GPIO.IN)
-    GPIO.setup(line_pin_left, GPIO.IN)
+class LineFollower:
+    def __init__(self):
+        self.line_pin_right = 19
+        self.line_pin_middle = 16
+        self.line_pin_left = 20
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(line_pin_right, GPIO.IN)
+        GPIO.setup(line_pin_middle, GPIO.IN)
+        GPIO.setup(line_pin_left, GPIO.IN)
+
     # motor.setup()
-
-
-def run():
-    status_right = GPIO.input(line_pin_right)
-    status_middle = GPIO.input(line_pin_middle)
-    status_left = GPIO.input(line_pin_left)
-    print("LF3: %d   LF2: %d   LF1: %d\n" % (status_right, status_middle, status_left))
-    time.sleep(0.2)
+    def get_status(self):
+        status_right = GPIO.input(line_pin_right)
+        status_middle = GPIO.input(line_pin_middle)
+        status_left = GPIO.input(line_pin_left)
+        return status_right, status_middle, status_left
 
 
 if __name__ == "__main__":
-    try:
-        setup()
-        while True:
-            run()
-    except KeyboardInterrupt:
-        pass
+    line_follower = LineFollower()
+    while True:
+        status_right, status_middle, status_left = line_follower.get_status()
+        print(f"Right: {status_right}, Middle: {status_middle}, Left: {status_left}")
+        time.sleep(0.1)
