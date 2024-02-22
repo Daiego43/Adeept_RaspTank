1389a147afbf44e027dd33961e32d3b7865dc0b0
daiego43 <diedelcha@gmail.com>
2024-02-22 10:45:53 +0100

    Quitando cosas de docker

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Iniciar el servicio SSH
-set -e
-# custom_config
-sudo service ssh start
-sudo chmod 0660 /dev/i2c-1
-sudo chgrp i2c /dev/i2c-1
-source /opt/ros/humble/setup.bash
-exec "$@"
